41de6ddf31038dd4c37d62661ee19d53fa3379cd
Ian Vaz Araujo <ianaraujo15@gmail.com>
2025-03-25 08:26:58 -0300

    smoke test and changed task type (not working yet)

--- src/domain/train.py
+++ src/domain/train.py
@@ -1,4 +1,5 @@
 import json
+import random
 from typing import List, Dict
 
 from datasets import Dataset, DatasetDict
@@ -6,6 +7,7 @@ from peft import LoraConfig, get_peft_model, TaskType
 
 from transformers import (
     AutoTokenizer, 
+    PreTrainedTokenizer,
     AutoModelForMaskedLM, 
     TrainingArguments, 
     Trainer, 
@@ -15,9 +17,9 @@ from transformers import (
 
 class DomainTrainer:
 
-    def __init__(self, model_name: str, path: str):
+    def __init__(self, model_name: str, path: str, smoke_test: bool = False) -> None:
         self.path = path
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, do_lower_case=False)
         self.model = AutoModelForMaskedLM.from_pretrained(model_name)
 
         self.lora_config = LoraConfig(
@@ -26,7 +28,7 @@ class DomainTrainer:
             target_modules=["query", "value"], # BERT-based models
             lora_dropout=0.05,
             bias="none",
-            task_type=TaskType.MASKED_LM
+            task_type=TaskType.SEQ_2_SEQ_LM
         )
 
         self.training_args = TrainingArguments(
@@ -40,15 +42,32 @@ class DomainTrainer:
             logging_steps=100,
             save_steps=1000,
             remove_unused_columns=False,
-            fp16=True
+            fp16=True,
+            seed=55
         )
 
-    def _tokenize_function(self, examples):
+        if smoke_test:
+            # max_steps overrides num_train_epochs, so the training ends quickly.
+            self.training_args.max_steps = 10
+
+            self.training_args.per_device_train_batch_size = 4
+            self.training_args.per_device_eval_batch_size = 4
+
+            # turn off checkpoint saving for speed
+            self.training_args.save_steps = 999999
+
+            # rreduce or turn off logging
+            self.training_args.logging_steps = 5
+
+    def _tokenize_function(self, examples) -> PreTrainedTokenizer:
         return self.tokenizer(
-            examples["text"], padding="max_length", truncation=True, max_length=512
+            examples["text"], 
+            padding="longest", 
+            truncation=True, 
+            max_length=512
         )
 
-    def _read_training_data(path: str) -> List[Dict]:
+    def _read_training_data(self, path: str) -> List[Dict]:
         data: List[Dict] = []
 
         with open(path, 'r', encoding='utf-8') as f:
@@ -58,9 +77,13 @@ class DomainTrainer:
         return data
     
     def _train_test_split(self, data: List[Dict], test_size: float = 0.2) -> DatasetDict:
+        
+        if self.training_args.max_steps == 10:
+            data = random.sample(data, 100)
+        
         dataset = Dataset.from_list(data)
 
-        train_test_split = dataset.train_test_split(test_size=test_size)
+        train_test_split = dataset.train_test_split(test_size=test_size, seed=55, shuffle=True)
         validation_test_split = train_test_split['test'].train_test_split(test_size=0.5)
 
         train = train_test_split['train']
@@ -73,7 +96,7 @@ class DomainTrainer:
             'test': test
         })
     
-    def train(self):
+    def train(self) -> None:
         # read data and create train/validation/test split
         data = self._read_training_data(self.path)
         dataset_dict = self._train_test_split(data)
@@ -82,7 +105,9 @@ class DomainTrainer:
         tokenized_dataset = dataset_dict.map(self._tokenize_function, batched=True)
 
         peft_model = get_peft_model(self.model, self.lora_config)
-        print('Trainable parameters: ', peft_model.print_trainable_parameters())
+
+        print("Trainable parameters:")
+        peft_model.print_trainable_parameters()
 
         data_collator = DataCollatorForLanguageModeling(
             tokenizer=self.tokenizer, 
@@ -102,12 +127,13 @@ class DomainTrainer:
         trainer.train()
 
         results = trainer.evaluate()
-        print(results)
+        print("Validation results:", results)
+
 
 if __name__ == '__main__':
     model_name = "neuralmind/bert-base-portuguese-cased"
     path = 'data/domain_training.jsonl'
 
-    trainer = DomainTrainer(model_name, path)
-    # trainer.train()
+    trainer = DomainTrainer(model_name, path, smoke_test=True)
+    trainer.train()
 
