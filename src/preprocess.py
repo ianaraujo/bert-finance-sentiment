1dee4d1d76e761754370c5f21d9b08f9e34e7438
Ian Vaz Araujo <ianaraujo15@gmail.com>
2025-03-29 22:22:45 -0300

    Add support for not punctuated strings

--- src/preprocess.py
+++ src/preprocess.py
@@ -9,16 +9,14 @@ conn = sqlite3.connect('letters.db')
 cursor = conn.cursor()
 
 split_pattern = re.compile(r'(?<=[.!?])(?:["”’\)\]]+)?\s+')
-token_pattern = re.compile(r'\w+')
+token_pattern = re.compile(r'\S+')
 
 def read_data() -> List:
     cursor.execute(
         '''
-        SELECT content 
+        SELECT gestora, title, content 
         FROM letters
-        WHERE gestora <> 'Encore'
-        AND content IS NOT NULL 
-        AND content <> ''
+        WHERE content IS NOT NULL AND content <> ''
         '''
     )
     data = cursor.fetchall()
@@ -26,18 +24,46 @@ def read_data() -> List:
     return data
 
 def sanitize_text(text: str) -> str:
+    text = text.replace("-\n", "")
     text = text.replace("\n", " ")
     text = " ".join(text.split())
     text = text.strip()
     
     return text
 
+def split_transcription(text: str, chunk_size: int = 500, overlap: int = 5) -> List[str]:
+    tokens = token_pattern.findall(text)
+
+    start = 0
+    chunks = []
+    total_tokens = len(tokens)
+
+    while start < total_tokens:
+        end = start + chunk_size
+
+        chunk_tokens = tokens[start:end]
+        chunk_text = ' '.join(chunk_tokens)
+        
+        chunks.append(chunk_text)
+
+        if end >= total_tokens:
+            break
+
+        start += (chunk_size - overlap)
+
+    return chunks
+
 def generate_chunks(data: List, min_size: int = 10, max_size: int = 500, threshold: float = 0.1) -> List[Dict]:
     chunks: List[Dict] = []
     
     for row in data:
-        text = row[0]
-        sentences = split_pattern.split(text)
+        text = row[2]
+        gestora, title = row[0], row[1]
+
+        if gestora == 'Encore' and 'Comentário' in title:
+            sentences = split_transcription(text)
+        else:
+            sentences = split_pattern.split(text)
 
         for sentence in sentences:
             sentence = sanitize_text(sentence)
@@ -66,17 +92,17 @@ def generate_chunks(data: List, min_size: int = 10, max_size: int = 500, thresho
 
 if __name__ == '__main__':
     data = read_data()
-    final_data = generate_chunks(data)
+    chunks_data = generate_chunks(data)
 
     for i in range(11):
-        print(f'Example {i}: ', random.choice(final_data))
+        print(f'Example {i}: ', random.choice(chunks_data))
 
-    print(f"Generated {len(final_data)} chunks")
+    print(f"Generated {len(chunks_data)} chunks")
 
     os.makedirs('data', exist_ok=True)
 
     with open('data/domain_training.jsonl', 'w', encoding='utf-8') as f:
-        for chunk in final_data:
+        for chunk in chunks_data:
             f.write(json.dumps(chunk, ensure_ascii=False) + '\n')
 
     print('Data saved to data/domain_training.jsonl')
